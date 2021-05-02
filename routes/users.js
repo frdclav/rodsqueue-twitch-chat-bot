e8699241cdc53badba8d2c4df934914ee2e971d4
frdclav <frclav@gmail.com>
2021-05-02 00:42:51 -0700

    changing the upi utils to hit the express server

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,102 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
+import axios from 'axios';
+import { roundToNearestMinutes } from 'date-fns';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const ID = function ()
+{
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return "_" + Math.random().toString( 36 ).substr( 2, 9 );
+};
 
+
+
+router.post( '/addtoqueueapi', ( req, res, next ) =>
+{
+  const value = req.body
+  axios.post( `${process.env.REACT_APP_FIREBASE_REST_API
+    }/seanthenkyle/curQueueArr.json`, {
+    id: ID(),
+    value
+  } ).then( ( response ) =>
+  {
+    res.send( response )
+  } )
+} )
+
+
+
+router.post( '/removefromqueueapi', ( req, res ) =>
+{
+  const value = req.body
+  axios.delete( `${process.env.REACT_APP_FIREBASE_REST_API
+    }/seanthenkyle/curQueueArr/${value}.json` ).then( ( response ) =>
+    {
+      res.send( response )
+    } )
+} )
+
+
+
+
+router.post( '/checkifuserexists', ( req, res, next ) =>
+{
+  const value = req.body
+  axios.get( `${process.env.REACT_APP_FIREBASE_REST_API
+    }/users/.json?orderBy="email"&startAt="${value}"` ).then( ( response ) =>
+    {
+      console.log( 'response.data', Object.keys( response.data ).length > 0, response )
+      res.send( response )
+      return response.data
+    } )
+} )
+
+router.post( '/createnewuser', ( req, res, next ) =>
+{
+  const value = req.body
+  axios.get( `${process.env.REACT_APP_FIREBASE_REST_API}/users/${value.uid}.json` ).then( ( response ) =>
+  {
+    const snap = response.data
+    console.log( 'createNewUser', response )
+    if ( snap )
+    {
+
+      res.send( response )
+      return console.log( 'user already exists', response )
+
+
+    } else
+    {
+      console.log( 'user does not exist', response )
+      axios.post( `${process.env.REACT_APP_FIREBASE_REST_API}/users/${value.uid}.json`, { value } ).then( ( response ) =>
+      {
+        res.send( response )
+        return console.log( 'user created', response.data )
+      } )
+    }
+
+
+  } )
+} )
+
+
+router.get( '/checkifuserlinkedtostore', ( req, res, next ) =>
+{
+  axios.get( `${process.env.REACT_APP_FIREBASE_REST_API}/users/${value.uid}/store.json` ).then( ( response ) =>
+  {
+    res.send( response )
+  } )
+} )
+
+
+router.post( '/setstore', ( req, res, next ) =>
+{
+  const value = req.body
+  const { store } = value
+  axios.post( `${process.env.REACT_APP_FIREBASE_REST_API}/users/${value.uid}/store.json`, { store } ).then( ( response ) => { res.send( response ) } )
+
+} )
 module.exports = router;
+
