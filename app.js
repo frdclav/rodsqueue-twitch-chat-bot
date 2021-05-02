573575debe9c79f5db38c62bc5f520e7b28780a1
frdclav <frclav@gmail.com>
2021-05-02 01:05:22 -0700

    remove if condition for express.static

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ app.use( express.urlencoded( { extended: true } ) );
 app.use( express.json() );
 app.use( cors() );
 // Serve up static assets (usually on heroku)
-if ( process.env.NODE_ENV === "production" )
-{
-  app.use( express.static( "client/build" ) );
-}
+// if ( process.env.NODE_ENV === "production" )
+// {
+app.use( express.static( "client/build" ) );
 // Add routes, both API and view
 // app.use( routes );
 app.use( '/firebaseAPI', firebaseAPIRouter );
