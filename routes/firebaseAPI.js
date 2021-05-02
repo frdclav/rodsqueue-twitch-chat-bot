d55dc48e69893aff2818e740a32f9932529b0498
frdclav <frclav@gmail.com>
2021-05-02 04:08:39 -0700

    add even more logging

--- routes/firebaseAPI.js
+++ routes/firebaseAPI.js
@@ -97,9 +97,13 @@ router.post( '/checkifuserlinkedtostore', ( req, res, next ) =>
 
   axios.get( `${firebaseURL}/users/${value.uid}/store.json` ).then( ( response ) =>
   {
-    console.log( 'check link 2', response )
+    console.log( 'check link 2', response.config, )
     res.send( response.data )
-  } ).catch( ( err ) => res.send( new Error( err ) ) )
+  } ).catch( ( err ) =>
+  {
+    console.log( 'error link 2' )
+    res.send( new Error( err ) )
+  } )
 } )
 
 
