ec97abd7c4d7184711cbd0287e8043c33bb5434c
frdclav <frclav@gmail.com>
2021-05-02 01:00:10 -0700

    add firebaseAPI line

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ if ( process.env.NODE_ENV === "production" )
   app.use( express.static( "client/build" ) );
 }
 // Add routes, both API and view
-app.use( routes );
+// app.use( routes );
+app.use( '/firebaseAPI', firebaseAPIRouter );
+
 
 
 // Start the API server
